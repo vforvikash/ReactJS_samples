f08a129c018f733223962222f59469f6c5d189a0
Training_h3b.09.09 <Training_H3B.09.09@accenture.com>
2016-06-30 23:51:19 +0530

    format change

--- Day-04/03-Redux/containers/App.js
+++ Day-04/03-Redux/containers/App.js
@@ -1,11 +1,7 @@
 import React, { Component, PropTypes } from 'react'
-
 import {bindActionCreators} from 'redux';
-
-
 import { connect } from 'react-redux'
 import Header from '../components/Header'
-
 import MainSection from '../components/MainSection'
 import Calculator from '../components/Calculator'
 import * as TodoActions from '../actions'
